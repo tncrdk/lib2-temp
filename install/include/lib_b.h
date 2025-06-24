9dce1748b461275d311bd7d087f24c886f6c0548
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-06-24 14:41:23 +0200

    removed build and install from tracking

--- install/include/lib_b.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace lib_b {
-    std::string get_message_b();
-} // namespace lib_b
-
--- install/include/lib_b2.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lib_b {
-    int multiply_numbers(int x, int y);
-} // namespace lib_b
-
