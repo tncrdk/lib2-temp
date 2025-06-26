eaa2dd73b809dec341f6d85237cbe3c8470b7e24
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-06-26 11:18:47 +0200

    added multiply two

--- lib_b2.cpp
+++ lib_b2.cpp
@@ -6,4 +6,7 @@ namespace lib_b {
     int multiply_numbers(int x, int y) {
         return x * y;
     }
+    int multiply_2(int x) {
+        return x * 2;
+    }
 } // namespace lib_b
--- lib_b2.h
+++ lib_b2.h
@@ -2,5 +2,6 @@
 
 namespace lib_b {
     int multiply_numbers(int x, int y);
+    int multiply_2(int x);
 } // namespace lib_b
 
